2206adb2820c93b43a4d282662a1d9abe4fe0e41
hajimxmiku <anakhandsomee@gmail.com>
2017-07-22 23:31:23 +0800

    Update Debian-kvm.sh

--- Debian-kvm.sh
+++ Debian-kvm.sh
@@ -3,6 +3,10 @@
 # go to root
 cd
 
+#check for update
+apt-get update
+apt-get upgrade
+
 # disable ipv6
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
